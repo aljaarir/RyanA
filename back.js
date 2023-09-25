4df2e315046cd2e2fd7db9cfca4622636e08adf5
aljaarir <aljaarir@msu.edu>
2023-09-25 19:38:28 -0400

    first commit

---
+++ back.js
@@ -0,0 +1,15 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
+// JavaScript for the interactive box
+
+document.getElementById("projects-button1").addEventListener("click", function() {
+  window.location.href = "https://onecompiler.com/mysql/3znf7rbp";
+});
+
+document.getElementById("projects-button2").addEventListener("click", function() {
+  window.location.href = "https://replit.com/@pyro888/EmployeePay";
+});
+
+document.getElementById("resume-button").addEventListener("click", function() {
+  window.location.href = "https://docs.google.com/document/d/e/2PACX-1vTlEkvNrWOZN-uZifrMzWASVLgISPYgxj54ffNRoky25poBNNPpM-jrr_JQlNhaLw/pub";
+});
