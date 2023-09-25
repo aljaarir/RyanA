9abea9e5f757fc560922b5b160d84bac62f262d5
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-09-25 19:57:50 -0400

    back.js

--- back.js
+++ back.js
@@ -1,15 +1,15 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
 
 // JavaScript for the interactive box
-
 document.getElementById("projects-button1").addEventListener("click", function() {
-  window.location.href = "https://onecompiler.com/mysql/3znf7rbp";
+  window.open("https://onecompiler.com/mysql/3znf7rbp", "_blank");
 });
 
 document.getElementById("projects-button2").addEventListener("click", function() {
-  window.location.href = "https://replit.com/@pyro888/EmployeePay";
+  window.open("https://replit.com/@pyro888/EmployeePay", "_blank");
 });
 
 document.getElementById("resume-button").addEventListener("click", function() {
-  window.location.href = "https://docs.google.com/document/d/e/2PACX-1vTlEkvNrWOZN-uZifrMzWASVLgISPYgxj54ffNRoky25poBNNPpM-jrr_JQlNhaLw/pub";
+  window.open("https://docs.google.com/document/d/e/2PACX-1vTlEkvNrWOZN-uZifrMzWASVLgISPYgxj54ffNRoky25poBNNPpM-jrr_JQlNhaLw/pub", "_blank");
 });
+
