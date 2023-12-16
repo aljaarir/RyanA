6436a7c3b85cf613bb6ede3036fd6f5351528991
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-12-16 11:07:34 -0500

    back.js

--- back.js
+++ back.js
@@ -1,54 +1,51 @@
-function redirectToCode(event) {
-    const projectBox = event.target.closest('.project-box');
-    if (projectBox) {
-        const codeUrl = projectBox.getAttribute('data-code-url');
-        if (codeUrl) {
-            window.location.href = codeUrl;
-        }
-    }
-}
-
-
-// Wait for the DOM to be ready
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     // Find the PROJECTS link in the navbar
     var projectsLink = document.querySelector('.navbar-right li a[href="#projects"]');
 
     // Add a click event listener to the PROJECTS link
     projectsLink.addEventListener('click', function (event) {
-      // Prevent the default behavior of the link (preventing the browser from navigating to the href)
-      event.preventDefault();
+        // Prevent the default behavior of the link (preventing the browser from navigating to the href)
+        event.preventDefault();
 
-      // Redirect to your desired HTML link
-      window.location.href = 'project.html';
+        // Redirect to your desired HTML link
+        window.location.href = 'projects.html';
     });
-  });
 
-// Wait for the DOM to be ready
-  document.addEventListener('DOMContentLoaded', function () {
-    // Find the PROJECTS link in the navbar
-    var projectsLink = document.querySelector('.navbar-right li a[href="#about"]');
+    // Find the ABOUT link in the navbar
+    var aboutLink = document.querySelector('.navbar-left a[href="#about"]');
 
-    // Add a click event listener to the PROJECTS link
-    projectsLink.addEventListener('click', function (event) {
-      // Prevent the default behavior of the link (preventing the browser from navigating to the href)
-      event.preventDefault();
+    // Add a click event listener to the ABOUT link
+    aboutLink.addEventListener('click', function (event) {
+        // Prevent the default behavior of the link (preventing the browser from navigating to the href)
+        event.preventDefault();
 
-      // Redirect to your desired HTML link
-      window.location.href = 'index.html';
+        // Redirect to your desired HTML link
+        window.location.href = 'index.html';
     });
-  });
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Find the RESUME link in the navbar
-  var resumeLink = document.querySelector('.navbar-right li a[href="#resume"]');
+    // Find the RESUME link in the navbar
+    var resumeLink = document.querySelector('.navbar-right li a[href="#resume"]');
 
-  // Add a click event listener to the RESUME link
-  resumeLink.addEventListener('click', function (event) {
-    // Prevent the default behavior of the link (preventing the browser from navigating to the href)
-    event.preventDefault();
+    // Add a click event listener to the RESUME link
+    resumeLink.addEventListener('click', function (event) {
+        // Prevent the default behavior of the link (preventing the browser from navigating to the href)
+        event.preventDefault();
 
-    // Replace 'your-resume-page.html' with the actual HTML page URL of your resume
-    window.location.href = 'https://drive.google.com/file/d/1my8AxZSocSSFcxLzeAPnwVOqRpUZshem/view?usp=sharing';
-  });
+        // Replace 'your-resume-page.html' with the actual HTML page URL of your resume
+        window.location.href = 'https://drive.google.com/file/d/1my8AxZSocSSFcxLzeAPnwVOqRpUZshem/view?usp=sharing';
+    });
+
+    // Find all project boxes and add click event listeners
+    var projectBoxes = document.querySelectorAll('.project-box');
+
+    projectBoxes.forEach(function (projectBox) {
+        projectBox.addEventListener('click', redirectToCode);
+    });
+
+    function redirectToCode(event) {
+        const codeUrl = event.currentTarget.getAttribute('data-code-url');
+        if (codeUrl) {
+            window.location.href = codeUrl;
+        }
+    }
 });
